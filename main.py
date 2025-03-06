8fa721bc38e6d327bfeddd101817084cce5b7be4
ConneelyS <sam.conneelywork@gmail.com>
2025-03-06 17:26:02 +0000

    Numpy NDArray maniuplation solution and implementation partially complete

--- main.py
+++ main.py
@@ -152,28 +152,53 @@ if __name__ == "__main__":
     # write_csv()
     # print(API_KEY)
 
-# Problem 3 (18 Marks)
+
+##### Problem 3 (18 Marks)
 
 array = np.random.randint(10, 101, size = (20, 5))
 
-print(array)
+## Print the full NDArray
+# print(array)
+
+## Printing the shape of the NDArray (rows, columns)
 # print(f"Rows: {array.shape[0]}\nColumns: {array.shape[1]}")
 
-# Checking if values in array are Even
+## Checking if values in array are Even
 # print(np.where(array % 2 == 0, True, False))
 
-# Checking sum of the values across rows
-print(sum(array))
-# print(array.sum(0))
-print(np.where(array.sum(0) % 2 == 0, True, False))
+## Printing the sum of each row in the NDArray
+## Checking the sum for each row using axis=1
+# print(array.sum(1))
 
-# if np.where(array.sum(0) % 2 == 0, True, False):
-#     array[-1] + 1
+row_sum_total = array.sum(1) # Returns a NDArray containing the sum of each row in array
+# print(row_sum_total)
 
-print(np.any(array, where = (array.sum(0) % 2 == 0)))
-print(array[0][0])
+odd_rows = row_sum_total % 2 # Returns a boolean mask NDArray for each row sum that is odd or even
+# print(odd_rows)
 
-# if np.any(array, where = (array.sum(0) % 2 == 0)):
-    # print(np.all(array.item))
-# print(np.any(array, 0, where = (array.sum(0) % 2 == 0)))
+random_odd = np.random.randint(0, 5, size = 20) # Selects a random int between low=0 and high=5 and stores in NDArray of length 20
+# print(random_odd)
 
+array[np.arange(20), random_odd] += odd_rows
+# array[x, y] += odd_rows(1 OR 0)
+# [0, 3] if odd + 1
+# [1, 2] if odd + 1
+# [2, 1] if even + 0
+# [3, 3] if even + 0
+# Numpy uses efficient broadcasting to avoid using IF statments
+
+## Checking total sum of NDArray
+array_sum_total = array.sum()
+print(array_sum_total)
+
+# Checking if array sum is divisible by 5
+array_modulo_remainder = array_sum_total % 5
+# print(f"Remaining: {array_modulo_remainder}")
+
+# Picking a random element in the inital NDArray to adjust the value of
+row = np.random.randint(0, 20)
+column = np.random.randint(0, 5)
+
+# Subtracting the modulo remainder from the element picked at random to make the sum divisible 
+array[row, column] -= array_modulo_remainder
+array_new_sum_total = array_sum_total - array_modulo_remainder
